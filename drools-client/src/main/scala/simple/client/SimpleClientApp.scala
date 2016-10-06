0b814b274aaef7bf9086c109854fab19548817aa
Raphael Parree <rparree@edc4it.com>
2016-10-06 08:55:06 +0300

    Updated dynamic client

--- drools-client/src/main/scala/simple/client/SimpleClientApp.scala
+++ drools-client/src/main/scala/simple/client/SimpleClientApp.scala
@@ -1,6 +1,7 @@
 package simple.client
 
 import org.kie.api.KieServices
+import org.kie.api.runtime.manager.RuntimeEnvironmentBuilderFactory
 import simple.model.Applicant
 
 import scala.annotation.tailrec
@@ -10,24 +11,26 @@ import scala.annotation.tailrec
  */
 object SimpleClientApp extends App {
 
-  private val services = KieServices.Factory.get
-  private val releaseId = services.newReleaseId("com.edc4it", "drools-core-demos", "1.0-SNAPSHOT")
-  private val kieContainer = services.newKieContainer(releaseId)
+    private val services = KieServices.Factory.get
+    private val releaseId = services.newReleaseId("com.edc4it",
+      "drools-core-demos", "[1.0,2)")
+    private val kieContainer = services.newKieContainer(releaseId)
 
-  val kScanner = services.newKieScanner(kieContainer)
-  kScanner.start(2000L)
-  
-  val kieSession = kieContainer.newStatelessKieSession("SimpleSession")
+    val kScanner = services.newKieScanner(kieContainer)
+    kScanner.start(2000L)
 
-  @tailrec
-  def check() {
-    val (aName, aAge) = scala.io.StdIn.readf2("{0} {1,number}")
-    val applicant = Applicant(name = aName.asInstanceOf[String], age = aAge.asInstanceOf[Long].toInt, pass = false)
-    kieSession.execute(applicant)
-    println(s"valid is ${applicant.pass}")
+    val kieSession = kieContainer.newStatelessKieSession("SimpleSession")
+
+    @tailrec
+    def check() {
+      val (aName, aAge) = scala.io.StdIn.readf2("{0} {1,number}")
+      val applicant = Applicant(name = aName.asInstanceOf[String], age = aAge.asInstanceOf[Long].toInt, pass = false)
+      kieSession.execute(applicant)
+      println(s"valid is ${applicant.pass}")
+      check()
+    }
+
     check()
-  }
-
-  check()
 
 }
+
